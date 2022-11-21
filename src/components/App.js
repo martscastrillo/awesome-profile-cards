cf768af55be0c0bbc92dbcf0f352bf14023351da
Sara Meixoeiro <sameidudu@gmail.com>
2022-11-21 12:11:52 +0100

    add react kit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Rellename!</h1>
+
+      {/* Esto es un comentario */}
+
+      {/* Aquí va tu código HTML. */}
+    </div>
+  );
+}
+
+export default App;
