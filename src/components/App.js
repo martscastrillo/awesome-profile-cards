b88b476907d236336f1f41e14a726bd8afc78258
Laura Garcia <lgarciacuello@gmail.com>
2022-11-22 12:13:30 +0100

    change class

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-
-import '../styles/App.scss';
-import logo from '../images/woman.png';
-import logoAdalab from '../images/logo-adalab.png';
+import "../styles/App.scss";
+import logo from "../images/woman.png";
+import logoAdalab from "../images/logo-adalab.png";
 
 function App() {
   return (
     <div>
-    <header className="header">
-      <a href="./index.html" className="header__link"><img src={logo}  alt="logo awesome profile-cards"  className="header__link--img
-"/></a>
-      <h1 className="header__title">Awesome profile-cards</h1>
-      
-    </header>
-    <main className="create">
+      <header className="header">
+        <a href="./index.html" className="header__link">
+          <img
+            src={logo}
+            alt="logo awesome profile-cards"
+            className="header__link--img
+"
+          />
+        </a>
+        <h1 className="header__title">Awesome profile-cards</h1>
+      </header>
+      <main className="create">
         <section className="card-section">
           <button className="reset js-reset">
             <i className="reset-icon fa-regular fa-trash-can"></i>
@@ -20,25 +24,42 @@ function App() {
           </button>
           <article className="card js-preview-card">
             <div className="card-text">
-              <h2 className="card-text-name js-preview-name">nombre apellido</h2>
-              <p className="card-text-job js-preview-job">front-end developer</p>
+              <h2 className="card-text-name js-preview-name">
+                nombre apellido
+              </h2>
+              <p className="card-text-job js-preview-job">
+                front-end developer
+              </p>
             </div>
-            <div
-              className="card-image js-card-img profile__image js__profile-image"
-              
-            ></div>
+            <div className="card-image js-card-img profile__image js__profile-image"></div>
 
             <div className="social-media">
-              <a href="#" className="social-media-icon js-phone-icon" target="_blank">
-                <i className="fa-solid fa-mobile-screen-button"></i
-              ></a>
-              <a href="#" className="social-media-icon js-email-icon" target="_blank">
+              <a
+                href="#"
+                className="social-media-icon js-phone-icon"
+                target="_blank"
+              >
+                <i className="fa-solid fa-mobile-screen-button"></i>
+              </a>
+              <a
+                href="#"
+                className="social-media-icon js-email-icon"
+                target="_blank"
+              >
                 <i className="fa-regular fa-envelope"></i>
               </a>
-              <a href="#" className="social-media-icon js-linkedin-icon" target="_blank">
+              <a
+                href="#"
+                className="social-media-icon js-linkedin-icon"
+                target="_blank"
+              >
                 <i className="fa-brands fa-linkedin-in"></i>
               </a>
-              <a href="#" className="social-media-icon js-github-icon" target="_blank">
+              <a
+                href="#"
+                className="social-media-icon js-github-icon"
+                target="_blank"
+              >
                 <i className="fa-brands fa-github-alt"></i>
               </a>
             </div>
@@ -51,20 +72,17 @@ function App() {
               <div className="design__div">
                 <i className="fa-solid fa-object-ungroup design__div--icon"></i>
                 <legend className="design__div--legend">diseña</legend>
-                <i
-                  className="fa fa-shield fa-shield-up design__div--arrow js-arrow js-arrow-design-up"
-                ></i>
-                <i
-                  className="fa fa-shield design__div--arrow js-arrow js-arrow-design-down collapsed"
-                ></i>
+                <i className="fa fa-shield fa-shield-up design__div--arrow js-arrow js-arrow-design-up"></i>
+                <i className="fa fa-shield design__div--arrow js-arrow js-arrow-design-down collapsed"></i>
               </div>
 
               <div className="design__second js-design">
-              
                 <div className="div2_container">
                   <div className="div2">
                     <ul className="div2__palette">
-                      <label className="div2__palette--checkBox">wonder adalaber</label>
+                      <label className="div2__palette--checkBox">
+                        wonder adalaber
+                      </label>
                       <input
                         type="radio"
                         id="green"
@@ -79,7 +97,9 @@ function App() {
                   </div>
                   <div className="div2">
                     <ul className="div2__palette">
-                      <label className="div2__palette--checkBox">super frontend</label>
+                      <label className="div2__palette--checkBox">
+                        super frontend
+                      </label>
                       <input
                         type="radio"
                         id="red"
@@ -94,7 +114,9 @@ function App() {
                   </div>
                   <div className="div2">
                     <ul className="div2__palette">
-                      <label className="div2__palette--checkBox">backend woman</label>
+                      <label className="div2__palette--checkBox">
+                        backend woman
+                      </label>
                       <input
                         type="radio"
                         id="random"
@@ -115,19 +137,18 @@ function App() {
               <div className="fill__container js-fill-title">
                 <i className="fa-regular fa-keyboard fill__container--icon"></i>
                 <legend className="fill__container--legend">rellena</legend>
-                <i
-                  className="fa fa-shield fill__container--arrow js-arrow js-arrow-fill-down"
-                ></i>
-                <i
-                  className="fa fa-shield fa-shield-up fill__container--arrow js-arrow js-arrow-fill-up collapsed"
-                ></i>
+                <i className="fa fa-shield fill__container--arrow js-arrow js-arrow-fill-down"></i>
+                <i className="fa fa-shield fa-shield-up fill__container--arrow js-arrow js-arrow-fill-up collapsed"></i>
               </div>
 
               <div className="js-fill collapsed">
                 <div className="fill__name">
-                  <label className="fill__name--label text-label" htmlFor="name"
-                    >nombre completo</label
+                  <label
+                    className="fill__name--label text-label"
+                    htmlFor="name"
                   >
+                    nombre completo
+                  </label>
                   <input
                     type="text"
                     name="name"
@@ -138,7 +159,9 @@ function App() {
                   />
                 </div>
                 <div className="fill__job">
-                  <label className="fill__job--label text-label" htmlFor="job">puesto</label>
+                  <label className="fill__job--label text-label" htmlFor="job">
+                    puesto
+                  </label>
                   <input
                     type="text"
                     id="job"
@@ -150,14 +173,15 @@ function App() {
                 </div>
 
                 <div className="fill__img">
-                  <label className="fill__img--label text-label" htmlFor="img"
-                    >imagen de perfil</label
-                  >
+                  <label className="fill__img--label text-label" htmlFor="img">
+                    imagen de perfil
+                  </label>
                   <label
                     className="fill__img--inputImg input js-input action__upload-btn"
                     htmlFor="img-selector"
-                    >añadir imagen</label
                   >
+                    añadir imagen
+                  </label>
                   <input
                     type="file"
                     name=""
@@ -167,7 +191,12 @@ function App() {
                 </div>
                 <div className="profile__preview js__profile-preview"></div>
                 <div className="fill__email">
-                  <label className="fill__email--label text-label" htmlFor="email">email</label>
+                  <label
+                    className="fill__email--label text-label"
+                    htmlFor="email"
+                  >
+                    email
+                  </label>
                   <input
                     type="email"
                     name="email"
@@ -177,7 +206,12 @@ function App() {
                   />
                 </div>
                 <div className="fill__tel">
-                  <label className="fill__tel--label text-label" htmlFor="phone">teléfono</label>
+                  <label
+                    className="fill__tel--label text-label"
+                    htmlFor="phone"
+                  >
+                    teléfono
+                  </label>
                   <input
                     type="tel"
                     name="phone"
@@ -187,9 +221,12 @@ function App() {
                   />
                 </div>
                 <div className="fill__linkedin">
-                  <label className="fill__linkedin--label text-label" htmlFor="linkedin"
-                    >linkedin</label
+                  <label
+                    className="fill__linkedin--label text-label"
+                    htmlFor="linkedin"
                   >
+                    linkedin
+                  </label>
                   <input
                     type="text"
                     name="linkedin"
@@ -199,7 +236,12 @@ function App() {
                   />
                 </div>
                 <div className="fill__git">
-                  <label className="fill__git--label text-label" htmlFor="github">github</label>
+                  <label
+                    className="fill__git--label text-label"
+                    htmlFor="github"
+                  >
+                    github
+                  </label>
                   <input
                     type="text"
                     name="github"
@@ -215,9 +257,7 @@ function App() {
               <div className="share__div">
                 <i className="fa-icon fa-solid fa-share-nodes share__div--icon"></i>
                 <legend className="share__div--legend">comparte</legend>
-                <i
-                  className="fa fa-shield fa-shield-up share__div--arrow js-arrow js-arrow-share-up collapsed"
-                ></i>
+                <i className="fa fa-shield fa-shield-up share__div--arrow js-arrow js-arrow-share-up collapsed"></i>
                 <i className="fa fa-shield share__div--arrow js-arrow js-arrow-share-down"></i>
               </div>
 
@@ -227,8 +267,14 @@ function App() {
               </button>
 
               <div className="share__card hidden js-share-card">
-                <h2 className="share__card--title">La tarjeta ha sido creada:</h2>
-                <a className="share__card--url js-link-card" href="#" target="_blank"></a>
+                <h2 className="share__card--title">
+                  La tarjeta ha sido creada:
+                </h2>
+                <a
+                  className="share__card--url js-link-card"
+                  href="#"
+                  target="_blank"
+                ></a>
 
                 <div className="share__twitter">
                   <a
@@ -243,20 +289,13 @@ function App() {
                 <p className="line"></p>
               </div>
             </fieldset>
-
           </form>
         </section>
-
-    </main>
-    <footer className="footer">
-      <p className="footer__copy">Awesome Women-cards @2022</p>
-      <img
-    class="footer__logo"
-    src={logoAdalab}
-    alt="Logo Adalab"
-  />
-    </footer>
-
+      </main>
+      <footer className="footer">
+        <p className="footer__copy">Awesome Women-cards @2022</p>
+        <img className="footer__logo" src={logoAdalab} alt="Logo Adalab" />
+      </footer>
     </div>
   );
 }
